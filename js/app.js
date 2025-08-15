b94e522a8ec4ece6385edfefa417a98382cafee7
iscid <iscidivandelgado@gmail.com>
2025-08-14 22:24:54 -0600

    Se completan validaciones del formulario

--- js/app.js
+++ js/app.js
@@ -3,13 +3,41 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
   //   Asignar eventos
-  inputEmail.addEventListener("blur", obtenerValor);
-  inputAsunto.addEventListener("blur", obtenerValor);
-  inputMensaje.addEventListener("blur", obtenerValor);
+  inputEmail.addEventListener("blur", validar);
+  inputAsunto.addEventListener("blur", validar);
+  inputMensaje.addEventListener("blur", validar);
 
-  function obtenerValor(e) {
-    console.log(e.target.value);
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+  }
+
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
+    // Genera la alerta
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+    // Inyectar el error en el formulario
+    referencia.appendChild(error);
+  }
+
+  function limpiarAlerta(referencia) {
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
+    }
   }
 });
