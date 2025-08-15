440660d63b92374d385b5aa5e36d9c94d581d924
iscid <iscidivandelgado@gmail.com>
2025-08-14 21:18:06 -0600

    Se inicializa el proyecto

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+
+  //   Asignar eventos
+  inputEmail.addEventListener("blur", obtenerValor);
+  inputAsunto.addEventListener("blur", obtenerValor);
+  inputMensaje.addEventListener("blur", obtenerValor);
+
+  function obtenerValor(e) {
+    console.log(e.target.value);
+  }
+});
